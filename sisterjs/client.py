825b789ea3365bf9ca8c27fd3cb707ce3dfd405c
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-08-14 23:35:05 +0700

    fix: zig loops, sisterjs base

---
+++ sisterjs/client.py
@@ -0,0 +1,32 @@
+import requests
+
+def main():
+    url = 'http://localhost:12345'
+    data = {'name': 'John'}
+
+    response = requests.get(url, data=data)
+
+    print("Get response status code:", response.status_code)
+    print("Response content:")
+    print(response.text)
+
+    response = requests.post(url, data=data)
+
+    print("Post response status code:", response.status_code)
+    print("Response content:")
+    print(response.text)
+
+    response = requests.put(url, data=data)
+    
+    print("Put response status code:", response.status_code)
+    print("Response content:")
+    print(response.text)
+
+    response = requests.delete(url, data=data)
+
+    print("Delete response status code:", response.status_code)
+    print("Response content:")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
