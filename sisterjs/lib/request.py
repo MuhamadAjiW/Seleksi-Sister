58b355262527a3295be6ed47803cc77c420c126a
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-08-16 18:51:53 +0700

    style: comments

--- sisterjs/lib/request.py
+++ sisterjs/lib/request.py
@@ -3,9 +3,6 @@ from lib.util import extract_wwwquery, extract_plaintext, extract_json
 # Request Class
 class Request():
     def __init__(self, reqstr:str=''):
-        # print("Received data:") # LOG
-        # print(reqstr) # LOG
-        
         reqstr = reqstr
         area = reqstr.split('\r\n\r\n')
         httplines = area[0].split('\r\n')
@@ -20,9 +17,6 @@ class Request():
         self.acc_type: str = ''
         self.content_length: int = 0
 
-        # print("Request addr: ", self.addr) # LOG
-        # print("Request type: ", self.type) # LOG
-
         for line in httplines:
             accloc = line.find('Accept:')
             contentloc = line.find('Content-Type:')
@@ -30,28 +24,18 @@ class Request():
 
             if(accloc != -1):
                 self.acc_type: str = line.split(': ')[1]
-                # print("Request accepts: ", end=' ') # LOG
-                # print(self.acc_type) # LOG
             
             if(contentloc != -1):
                 self.content_type: str = line.split(': ')[1]
-                # print("Request has content: ", end=' ') # LOG
-                # print(self.content_type)
-                # print("Content: ", area[1])
                 if(self.content_type == 'application/x-www-form-urlencoded'):
                     self.contents = extract_wwwquery(area[1])
                 elif(self.content_type == 'text/plain'):
                     self.contents = extract_plaintext(area[1])
                 elif(self.content_type == 'application/json'):
                     self.contents = extract_json(area[1])
-                # print("Request contents: ") # LOG
-                # print(self.contents) # LOG
             
             if(lengthloc != -1):
                 self.content_length: int = int(line.split(': ')[1])
-
-                # print("Request length: ") # LOG
-                # print(self.content_length) # LOG
 
         if(len(addr_cnt) > 1):
             self.query = extract_wwwquery(addr_cnt[1])
