41dd45c595121cac7f7489c461c234b15289f3ed
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-08-17 20:57:27 +0700

    feat: finalization

--- submit.py
+++ submit.py
@@ -1,6 +1,7 @@
 import pyotp
 import base64
 import hashlib
+import requests
 
 def generate_totp(secret_key):
     base32_secret = base64.b32encode(secret_key.encode('utf-8')).decode('utf-8')
@@ -19,3 +20,12 @@ upass = uid + ":" + totp_code
 print(upass)
 base64_upass = base64.b64encode(upass.encode('utf-8')).decode('utf-8')
 print(base64_upass)
+
+url = "http://recruit.sister20.tech/submit/b"
+data = {'fullName': 'Muhamad Aji Wibisono', 'link': "https://github.com/MuhamadAjiW/SeleksiSister23", "message":"https://www.youtube.com/watch?v=L-aviGyL4c4&ab_channel=brombo"}
+headers = {'Content-Type': 'application/json', 'Authorization': 'Basic ' + base64_upass}
+response = requests.post(url, json=data, headers=headers)
+
+print("\nstatus code:", response.status_code)
+print("Response content:")
+print(response.text)
